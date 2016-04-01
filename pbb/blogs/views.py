87803f9ccc752f3b7334aba1f838bb0f150dbbf2
Diego Garcia <drgarcia1986@gmail.com>
2016-03-31 22:14:57 -0300

    add success message in BlogUpdateView

--- pbb/blogs/views.py
+++ pbb/blogs/views.py
@@ -1,3 +1,4 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.core.urlresolvers import reverse
 from django.views.generic import ListView, UpdateView
 
@@ -8,9 +9,13 @@ class BlogListView(ListView):
     model = Blog
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(SuccessMessageMixin, UpdateView):
     model = Blog
     fields = ('feed_url',)
+    success_message = '{title} was updated successfully'
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message.format(title=self.object.title)
 
     def get_success_url(self):
         return reverse('blogs:list')
