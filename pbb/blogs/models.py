45bb8fd73168dae0b9752227b1e8bd23315f7245
Diego Garcia <drgarcia1986@gmail.com>
2016-04-01 22:05:52 -0300

    slugify blogs update link

--- pbb/blogs/models.py
+++ pbb/blogs/models.py
@@ -1,10 +1,21 @@
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.text import slugify
 
 from core.models import TimeStampedModel
 
 
 class Blog(TimeStampedModel):
-    title = models.CharField(max_length=200)
-    url = models.URLField(db_index=True)
+    title = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(unique=True)
+    url = models.URLField(unique=True)
     feed_url = models.URLField(null=True)
     working = models.BooleanField()
+
+    def get_absolute_url(self):
+        return reverse('blogs:update', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        super(Blog, self).save(*args, **kwargs)
--- pbb/blogs/urls.py
+++ pbb/blogs/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.BlogListView.as_view(), name='list'),
-    url(r'^(?P<pk>\d+)$', views.BlogUpdateView.as_view(), name='update'),
+    url(r'^(?P<slug>[\w-]+)$', views.BlogUpdateView.as_view(), name='update'),
 ]
